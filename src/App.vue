ac733f8d3b68e3aa5e79399c87caf050453f28b7
mansinakrani <mansinakrani006@gmail.com>
2024-01-18 12:13:35 +0530

    starting basic concept of ts in vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <div class="app">
-    Hello 
-  </div> -->
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,11 @@
 <script setup lang="ts">
-import { reactive, ref, toRefs } from 'vue';
-import {Job} from '@/types/Job'
-// import TheWelcome from '../components/TheWelcome.vue'
-// const name = ref('John');
-
-// const age = ref<number | string>(24); // generic argument for assigning types
-
-// return { name, age },
-
-// const state = reactive({
-//   name: 'John',
-//   age: 25 as string | number // type assertive used to assign var types
-// })
-
-// return { ...toRefs(state) }
-
-// methods: {
-//     changeName(name: string) {
-//       this.name = name
-//       return name
-//     },
-//     changeAge(age: number | string) {
-//       this.age = age
-//       return age
-//     }
-//   }
-
-const jobs = ref<Job[]>([
-      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-    ])
-    return { jobs }
+import TheWelcome from '../components/TheWelcome.vue'
 
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
+    <TheWelcome />
     <h3>HOME</h3>
-    <p>{{ name }} - {{ age }}</p>
-    <!-- <button @click="changeName('Zack')">change name</button>
-    <button @click="changeAge('30')">change age</button> -->
   </main>
 </template>
