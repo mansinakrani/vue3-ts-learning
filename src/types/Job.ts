23cdc100b38891bbdcfe7d7a79bea7b1a17f4914
mansinakrani <mansinakrani006@gmail.com>
2024-01-18 11:35:02 +0530

    first commit

---
+++ src/types/Job.ts
@@ -0,0 +1,10 @@
+// export default interface Job {
+  export interface Job {
+  title: string,
+  location: string,
+  salary: number,
+  id: string
+}
+
+// export type { Job }
+// export default Job
